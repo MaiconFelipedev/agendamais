5567b37d6ca730d498a2f378f30519434a2b9a2e
José Guilherme <jguilhermef.s2@gmail.com>
2025-01-10 18:13:38 -0300

    Adicionando Agendamento e Pagamento.

--- src/shared/model/agendamento.ts
+++ src/shared/model/agendamento.ts
@@ -1,1 +1,29 @@
-class Agendamento {}
+class Agendamento {
+ 
+    constructor(
+      private _data: Date,
+      private _horarioInicio: string,
+      private _horarioFinal: string,
+      private _cliente: Cliente,
+      private _servico: Servico,
+      private _valorTotal: number,
+      private _status: string = "Agendado",
+      private _id ?: number
+    ) {}
+  
+    cancelar(): void {
+      this._status = "Cancelado";
+      console.log(`Agendamento ${this._id} foi cancelado.`);
+    }
+  
+    reagendar(novaData: Date, novoHorarioInicio: string, novoHorarioFinal: string): void {
+      this._data = novaData;
+      this._horarioInicio = novoHorarioInicio;
+      this._horarioFinal = novoHorarioFinal;
+      this._status = "Reagendado";
+      console.log(
+        `Agendamento ${this._id} foi reagendado para ${this._data} às ${this._horarioInicio}-${this._horarioFinal}.`
+      );
+    }
+  }
+  
--- src/shared/model/pagamento.ts
+++ src/shared/model/pagamento.ts
@@ -1,1 +1,22 @@
-class Pagamento {}
+class Pagamento {
+  
+    constructor(
+    private _id: number,
+    private _agendamento: Agendamento,
+    private _metodo: string,
+    private _status: string,
+    private _valor: number
+    ) {}
+  
+    processarPagamento(): void {
+      
+      if (this._metodo && this._valor > 0) {
+        this._status = "Processado";
+        console.log(`Pagamento de R$${this._valor} processado com sucesso.`);
+      } else {
+        console.log("Falha ao processar pagamento.");
+      }
+    }
+  }
+  
+  
