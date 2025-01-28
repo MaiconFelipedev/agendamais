37e5c81ff2da608548b9a2f3fb1a1621ee4a40b0
Lucas Emiliano <lucasemiliano619@gmail.com>
2025-01-27 23:43:38 -0300

    Cria módulos e componentes
    Criação da pasta modules contendo o módulo de Serviço, que tem o componente de cadastro de serviços, e Usuário, que possui o componente de login de usuário.

---
+++ src/app/modules/servico/cadastro-servico/cadastro-servico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-servico',
+  imports: [],
+  templateUrl: './cadastro-servico.component.html',
+  styleUrl: './cadastro-servico.component.scss'
+})
+export class CadastroServicoComponent {
+
+}
