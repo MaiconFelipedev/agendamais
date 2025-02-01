f7a67f09f3d49b9062dae58ef5a2aacb177451b2
Lucas Emiliano <lucasemiliano619@gmail.com>
2025-02-01 20:05:44 -0300

    Cria botão de login e espaço para validar campos
    Criação de um botão que irá validar o login checando se o email e a senha existem no banco de dados, caso não existam apresentará uma mensagem de erro.

--- src/app/modules/usuario/login-usuario/login-usuario.component.ts
+++ src/app/modules/usuario/login-usuario/login-usuario.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import {MaterialModule} from '../../material/material.module';
-import {RouterLink} from '@angular/router';
+import { MaterialModule } from '../../material/material.module';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login-usuario',
   templateUrl: './login-usuario.component.html',
   standalone: true,
-  imports: [MaterialModule, RouterLink],
+  imports: [MaterialModule, RouterLink, FormsModule, NgIf],
   styleUrls: ['./login-usuario.component.scss']
 })
 export class LoginUsuarioComponent {
+  campoEmail: string = '';
+  campoSenha: string = '';
+
+  entrar(email: string, senha: string) {
+    console.log(email, senha)
+  }
 }
