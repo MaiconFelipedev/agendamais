8b0499acc4df40ef5fbdcf90c05e3e955995b989
Lucas Emiliano <lucasemiliano619@gmail.com>
2025-02-01 21:47:48 -0300

    Remove declarações desnecessárias nos módulos
    Removendo declarações e importações desnecessárias nos módulos, tendo em vista que os componentes são standalone.

--- src/app/modules/servico/servico.module.ts
+++ src/app/modules/servico/servico.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AgendamentoServicoComponent } from './agendamento-servico/agendamento-servico.component';
-import { CadastroServicoComponent } from './cadastro-servico/cadastro-servico.component';
-import { CardServicoComponent } from './card-servico/card-servico.component';
-import { ListagemServicosComponent } from './listagem-servicos/listagem-servicos.component';
 import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AgendamentoServicoComponent,
-    CadastroServicoComponent,
-    CardServicoComponent,
-    ListagemServicosComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [
-    AgendamentoServicoComponent,
-    CadastroServicoComponent,
-    CardServicoComponent,
-    ListagemServicosComponent
-  ]
+  exports: []
 })
 export class ServicoModule { }
--- src/app/modules/usuario/usuario.module.ts
+++ src/app/modules/usuario/usuario.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AgendaPrestadorComponent } from './agenda-prestador/agenda-prestador.component';
-import { AvaliacaoUsuarioComponent } from './avaliacao-usuario/avaliacao-usuario.component';
-import {CadastroUsuarioComponent} from './cadastro-usuario/cadastro-usuario.component';
 import {MaterialModule} from '../material/material.module';
 
 @NgModule({
-  declarations: [
-    AgendaPrestadorComponent,
-    AvaliacaoUsuarioComponent,
-    CadastroUsuarioComponent
-  ],
-  imports: [
-    CommonModule,
-  ],
-  exports: [
-    AgendaPrestadorComponent,
-    AvaliacaoUsuarioComponent,
-    CadastroUsuarioComponent,
-    MaterialModule
-  ]
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MaterialModule]
 })
 export class UsuarioModule { }
