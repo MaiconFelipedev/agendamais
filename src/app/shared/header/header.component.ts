15f39b7e92c350382f401697804fc00dc007c193
Lucas Emiliano <lucasemiliano619@gmail.com>
2025-02-03 01:20:11 -0300

    Alterna a exibição de elementos no header
    Checa se o usuário está logado, caso esteja, exibe seu nome, se não, mostra botão para fazer login.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import {MaterialModule} from '../../modules/material/material.module';
 import {RouterLink} from '@angular/router';
+import {UsuarioService} from '../../modules/usuario/usuario.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     MaterialModule,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './header.component.html',
   standalone: true,
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+constructor(
+    protected usuarioService: UsuarioService
+  ) {}
 }
