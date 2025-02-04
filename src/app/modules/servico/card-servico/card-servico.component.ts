48ecc0f296fe2530177d0e9f51f50686db146895
Sofia Leal <sofialealoliveira@gmail.com>
2025-02-04 13:17:38 -0300

    Atualiza componente de Card de Serviço

--- src/app/modules/servico/card-servico/card-servico.component.ts
+++ src/app/modules/servico/card-servico/card-servico.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {Component, ElementRef, inject, Input, ViewChild} from '@angular/core';
 import {MaterialModule} from '../../material/material.module';
 import {CommonModule} from '@angular/common';
 import {Servico} from '../../../shared/model/servico';
 import {Title} from '@angular/platform-browser';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-card-servico',
   templateUrl: './card-servico.component.html',
   styleUrl: './card-servico.component.scss',
   standalone: true,
-  imports: [MaterialModule, CommonModule]
+  imports: [MaterialModule, CommonModule, RouterLink]
 })
 export class CardServicoComponent {
   @Input() servico!: Servico;
