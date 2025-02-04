d3cf7a5d876341c730c77c205eea200ae80a9ea6
Sofia Leal <sofialealoliveira@gmail.com>
2025-02-04 13:24:30 -0300

    Cria Service de Agenda

--- src/app/modules/usuario/agenda.service.ts
+++ src/app/modules/usuario/agenda.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {Agenda} from '../../shared/model/agenda';
+import {DiaDeTrabalho} from '../../shared/model/dia-trabalho';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendaService {
   private agendas: Agenda[] = [];
+  public agendaAtual: Agenda | undefined;
   private nextId = 1;
 
   constructor() { }
@@ -17,9 +19,14 @@ export class AgendaService {
   salvarAgenda(agenda: Agenda): void {
     agenda.id = this.gerarId();
     this.agendas.push(agenda);
+    this.agendaAtual = agenda;
   }
 
-  resgatarPorId(idPrestador: number | undefined): Agenda | undefined {
-    return this.agendas.find(agenda => agenda.idPrestador === idPrestador)
+  resgatarAgenda(): Agenda | undefined{
+    return this.agendaAtual;
+  }
+
+  resgatarDiaTrabalho(data: string): DiaDeTrabalho | undefined{
+    return this.agendaAtual?.diasDeTrabalho.filter(diaDeTrabalho => diaDeTrabalho.dataFormatada === data)[0];
   }
 }
